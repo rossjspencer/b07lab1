c8a432ec22e10a73d00e7d5d0865abd1762f0f44
Ross Spencer <ross.j.spencer@gmail.com>
2025-09-21 15:04:58 -0400

    "added Polynomial.java and Driver.java"

---
+++ Polynomial.java
@@ -0,0 +1,58 @@
+import static java.lang.Math.max;
+import static java.lang.Math.pow;
+
+public class Polynomial
+{
+    double[] coefficients;
+
+    public Polynomial ()
+    {
+        coefficients = new double[]{0};
+    }
+
+    public Polynomial (double[] input)
+    {
+        coefficients = input;
+    }
+
+    public Polynomial add(Polynomial p)
+    {
+        if(max(p.coefficients.length, this.coefficients.length) == p.coefficients.length)
+        {
+            //add polynomials - p is the longer list
+            for(int i = 0; i < this.coefficients.length; i++)
+            {
+                p.coefficients[i] = this.coefficients[i] + p.coefficients[i];
+            }
+
+            return p;
+        }
+        else
+        {
+            //add polynomials - this is the longer list
+            for(int i = 0; i < p.coefficients.length; i++)
+            {
+                this.coefficients[i] = this.coefficients[i] + p.coefficients[i];
+            }
+
+            return this;
+        }
+    }
+
+    public double evaluate(double x)
+    {
+        double result = 0;
+
+        for(int i = 0; i < this.coefficients.length; i++)
+        {
+            result += this.coefficients[i] * pow(x, i);
+        }
+
+        return result;
+    }
+
+    public boolean hasRoot(double x)
+    {
+        return this.evaluate(x) == 0.0;
+    }
+}
