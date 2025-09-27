afaefa32b55c1322dcceed52496183d29b8b5e93
Ross Spencer <ross.j.spencer@gmail.com>
2025-09-27 11:20:24 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,17 +1,36 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Driver
 {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws IOException
+    {
         Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
+
+        double[] c1 = {6,5};
+        int[] e1 = {0, 3};
+        Polynomial p1 = new Polynomial(c1, e1);
+
+        double[] c2 = {-9,-2};
+        int[] e2 = {4, 1};
+        Polynomial p2 = new Polynomial(c2, e2);
+
+        Polynomial p3 = new Polynomial(new File("p3file.txt"));
+        System.out.println(p3.evaluate(2));
+
+        Polynomial p4 = p3.multiply(p2);
+        Polynomial p5 = p3.add(p2);
+
+        p4.saveToFile("multiplication.txt");
+        p5.saveToFile("addition_out_of_order.txt");
+
         Polynomial s = p1.add(p2);
         System.out.println("s(0.1) = " + s.evaluate(0.1));
         if(s.hasRoot(1))
             System.out.println("1 is a root of s");
         else
             System.out.println("1 is not a root of s");
+
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,42 +1,156 @@
-import static java.lang.Math.max;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+import static java.lang.Double.parseDouble;
 import static java.lang.Math.pow;
 
 public class Polynomial
 {
     double[] coefficients;
+    int[] exponents;
 
-    public Polynomial ()
+    public Polynomial()
     {
-        coefficients = new double[]{0};
+        coefficients = new double[]{};
+        exponents = new int[]{};
     }
 
-    public Polynomial (double[] input)
+    public Polynomial(double[] coefficients, int[] exponents)
     {
-        coefficients = input;
+        this.coefficients = coefficients;
+        this.exponents = exponents;
+    }
+
+    public Polynomial(File file) throws FileNotFoundException
+    {
+        Scanner reader = new Scanner(file);
+        String polynomialString = reader.nextLine();
+
+        //this regex made me pull my hair out
+        //splits polynomial string into individual terms
+        String[] polynomialStrings = polynomialString.split("(?=[+\\-])");
+
+        double[] coefficients = new double[polynomialStrings.length];
+        int[] exponents = new int[polynomialStrings.length];
+
+        for(int i = 0; i < polynomialStrings.length; i++)
+        {
+            if(!polynomialStrings[i].contains("x"))
+            {
+                coefficients[i] = parseDouble(polynomialStrings[i]);
+                exponents[i] = 0;
+            }
+            else
+            {
+                String[] splitOnX = polynomialStrings[i].split("x");
+                coefficients[i] = parseDouble(splitOnX[0]);
+                exponents[i] = Integer.parseInt(splitOnX[1]);
+            }
+        }
+
+        this.coefficients = coefficients;
+        this.exponents = exponents;
+    }
+
+    public void saveToFile(String filename) throws IOException
+    {
+        String polynomialString = "";
+
+        for(int i = 0; i < this.coefficients.length; i++)
+        {
+            //polynomial string does not display x0
+            if(this.exponents[i] == 0)
+            {
+                polynomialString += this.coefficients[i];
+            }
+            else
+            {
+                //positive coefficient and empty string
+                if(this.coefficients[i] > 0 && polynomialString.isEmpty())
+                {
+                    polynomialString += this.coefficients[i] + "x" + this.exponents[i];
+                }
+                //positive coefficient with nonempty string
+                else if (this.coefficients[i] > 0)
+                {
+                    polynomialString += "+" + this.coefficients[i] + "x" + this.exponents[i];
+                }
+                //the - is included in the coefficient double
+                else
+                {
+                    polynomialString += this.coefficients[i] + "x" + this.exponents[i];
+                }
+            }
+        }
+
+        //set up file writer
+        FileWriter writer = new FileWriter(filename);
+
+        writer.write(polynomialString);
+        writer.close();
     }
 
     public Polynomial add(Polynomial p)
     {
-        if(max(p.coefficients.length, this.coefficients.length) == p.coefficients.length)
-        {
-            //add polynomials when p is the longer list
-            for(int i = 0; i < this.coefficients.length; i++)
-            {
-                p.coefficients[i] = this.coefficients[i] + p.coefficients[i];
-            }
+        //keeps coefficients and exponents parallel
+        //key system automatically adds coefficients when adding to map - handy!
+        Map<Integer, Double> polynomialMap = new HashMap<>();
 
-            return p;
-        }
-        else
+        //add this to map
+        for (int i = 0; i < this.coefficients.length; i++)
         {
-            //add polynomials when this is the longer list
-            for(int i = 0; i < p.coefficients.length; i++)
-            {
-                this.coefficients[i] = this.coefficients[i] + p.coefficients[i];
-            }
-
-            return this;
+            polynomialMap.put(this.exponents[i],
+                    polynomialMap.getOrDefault(this.exponents[i], 0.0) + this.coefficients[i]);
         }
+
+        //add p to map
+        for (int i = 0; i < p.coefficients.length; i++)
+        {
+            polynomialMap.put(p.exponents[i],
+                    polynomialMap.getOrDefault(p.exponents[i], 0.0) + p.coefficients[i]);
+        }
+
+        //clear zero coefficients
+        polynomialMap.entrySet().removeIf(e -> e.getValue() == 0.0);
+
+        //bonus step: output polynomials will be sorted and pretty
+        List<Map.Entry<Integer, Double>> prettyPolynomial = new ArrayList<>(polynomialMap.entrySet());
+        prettyPolynomial.sort(Comparator.comparingInt(Map.Entry::getKey));
+
+        //return to arrays
+        int size = prettyPolynomial.size();
+        double[] coefficients = new double[size];
+        int[] exponents = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            exponents[i] = prettyPolynomial.get(i).getKey();
+            coefficients[i] = prettyPolynomial.get(i).getValue();
+        }
+
+        return new Polynomial(coefficients, exponents);
+    }
+
+    //the new implementation of the add method makes this wonderfully simple
+    public Polynomial multiply(Polynomial p)
+    {
+        Polynomial result = new Polynomial();
+
+        for(int i = 0; i < this.coefficients.length; i++)
+        {
+            for(int j = 0; j < p.coefficients.length; j++)
+            {
+                int[] exponent = {this.exponents[i] + p.exponents[j]};
+                double[] coefficient = {this.coefficients[i] * p.coefficients[j]};
+
+                result = result.add(new Polynomial(coefficient, exponent));
+            }
+        }
+
+        return result;
     }
 
     public double evaluate(double x)
@@ -45,7 +159,7 @@ public class Polynomial
 
         for(int i = 0; i < this.coefficients.length; i++)
         {
-            result += this.coefficients[i] * pow(x, i);
+            result += this.coefficients[i] * pow(x, this.exponents[i]);
         }
 
         return result;
